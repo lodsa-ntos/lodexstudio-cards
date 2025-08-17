9ba045658ff6b1ebbc2458373b7bdd7278f06392
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-17 01:42:29 +0100

    Update: 2025-08-17 01:42:29 [src/components/HeroSection.jsx, src/components/cards/ShimmerBorderCard.jsx]

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import GlobalContainer from "../utils/GlobalContainer";
 import SwipeCards from "./cards/SwipeCards";
 import TiltShineCard from "./cards/TiltShineCard";
+import ShimmerBorderCard from "./cards/ShimmerBorderCard";
 
 function HeroSection() {
   return (
     <GlobalContainer>
-      <div className="flex items-center justify-center min-h-screen ">
-        <TiltShineCard />
-        {/* <SwipeCards /> */}  
+      <div className="flex items-center justify-center min-h-screen bg-zinc-950 text-white">
+        <ShimmerBorderCard>
+          <p className="relative z-10 mb-4 w-full text-3xl font-bold text-slate-50">Shimmer Border Card</p>
+          <p className="relative z-10 text-slate-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea quaerat est iusto praesentium reiciendis minima numquam iste, quos nostrum impedit sed natus corrupti aspernatur aperiam beatae, aliquid, veritatis deleniti? Id!</p>
+        </ShimmerBorderCard>
+        {/* <TiltShineCard /> */}
+        {/* <SwipeCards /> */}
       </div>
     </GlobalContainer>
   );

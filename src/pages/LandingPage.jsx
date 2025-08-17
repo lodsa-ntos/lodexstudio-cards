ed670d8fb84f259ae88398c420d942ad128cc895
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-17 18:21:01 +0100

    Update: 2025-08-17 18:21:01 [src/pages/LandingPage.jsx]

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import Hero from "../components/HeroSection";
-import { Link } from "react-router-dom";
+import DemoGrid from "./DemoGrid";
 
 function LandingPage() {
   return (
     <>
-      <Hero />
-      <div className="flex justify-center mt-8">
-        <Link
-          to="/demo"
-          className="px-6 py-3 bg-indigo-600 text-white rounded-xl font-semibold shadow-lg hover:bg-indigo-700 transition-colors"
-        >
-          View All Cards Demo
-        </Link>
-      </div>
+      <DemoGrid />
     </>
   );
 }
 
 export default LandingPage;
+

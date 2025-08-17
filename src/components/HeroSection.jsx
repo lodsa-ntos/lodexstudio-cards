39a2d33070b49b4566ae25edfd63c1fa92573440
Lodney Santos <a21505293@alunos.ulht.pt>
2025-08-17 02:00:13 +0100

    Update: 2025-08-17 02:00:13 [src/components/HeroSection.jsx, src/components/cards/DragCards.jsx]

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,18 +3,22 @@ import GlobalContainer from "../utils/GlobalContainer";
 import SwipeCards from "./cards/SwipeCards";
 import TiltShineCard from "./cards/TiltShineCard";
 import ShimmerBorderCard from "./cards/ShimmerBorderCard";
+import DragCards from "./cards/DragCards";
 
 function HeroSection() {
   return (
     <GlobalContainer>
-      <div className="flex items-center justify-center min-h-screen bg-zinc-950 text-white">
+      <section className="flex items-center justify-center min-h-screen bg-neutral-950 text-white">
+        <DragCards />
+        {/* 
         <ShimmerBorderCard>
           <p className="relative z-10 mb-4 w-full text-3xl font-bold text-slate-50">Shimmer Border Card</p>
           <p className="relative z-10 text-slate-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea quaerat est iusto praesentium reiciendis minima numquam iste, quos nostrum impedit sed natus corrupti aspernatur aperiam beatae, aliquid, veritatis deleniti? Id!</p>
         </ShimmerBorderCard>
+        */}
         {/* <TiltShineCard /> */}
         {/* <SwipeCards /> */}
-      </div>
+      </section>
     </GlobalContainer>
   );
 }
---
+++ src/components/cards/DragCards.jsx
@@ -0,0 +1,15 @@
+import React, { useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { twMerge } from "tailwind-merge";
+
+function DragCards() {
+  return (
+    <div className="relative grid w-full place-content-center overflow-hidden min-h-screen">
+      <h2 className="relative z-0 text-[20vw] font-black text-neutral-800 md:text-[199px]">LodeX Studio</h2>
+      
+    </div>
+  );
+}
+
+
+export default DragCards;
